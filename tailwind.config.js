9034446439294f052f7fd51b206189bd0f890475
Robert Guthrie <rob@guthrie.nz>
2023-06-18 13:24:58 +1200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
